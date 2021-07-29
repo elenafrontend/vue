b6d3dc764b17c7a5dfd2be0a40fbbeb858654f1b
gumerova.e <gumerova.e@etton.ru>
2021-07-29 14:23:49 +0300

    lesson-4: reactivity - add new property to data object

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <UserForm></UserForm>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserForm from './components/user-form'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserForm
   }
 }
 </script>
