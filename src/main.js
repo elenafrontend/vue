8edc5d151e1c073c66fdf290a4136a3f618aa110
gumerova.e <gumerova.e@etton.ru>
2021-08-02 17:32:46 +0300

    lesson-9: vuex install

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router/index'
+import store from './store/index'
 
 Vue.use(VueRouter)
 
@@ -9,5 +10,6 @@ import '@/assets/styles/main.css'
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app')
