6b9c3131d5536095523aee73f135bb55d0a86b7d
gumerova.e <gumerova.e@etton.ru>
2021-08-02 14:06:14 +0300

    lesson-8: add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="not-found">
+    <h1>Ничего не найдено</h1>
+  </div>
+</template>
+
+<style>
+.not-found {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.not-found h1 {
+  color: #ffffff;
+  font-size: 70px;
+  margin-bottom: 20px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import MainPage from '../pages/MainPage'
 import AllFilmsPage from '../pages/AllFilmsPage'
 import FilmPage from '../pages/FilmPage'
+import NotFound from '../pages/404'
 
 export default new VueRouter({
   routes: [
@@ -21,6 +22,11 @@ export default new VueRouter({
       path: '/films/:id',
       name: 'filmPage',
       component: FilmPage
+    },
+    {
+      path: '*',
+      name: 'notFound',
+      component: NotFound
     },
   ]
 })
