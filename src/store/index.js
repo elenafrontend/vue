47e1155f4e462cac36cc340ec74179e0b90db5a8
gumerova.e <gumerova.e@etton.ru>
2021-08-05 09:38:04 +0300

    lesson-9: add vuex modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,41 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// импортируем данные 'сервера'
-import reactions from '../mocks/reactions'
+import reactions from './modules/reactions'
 
 Vue.use(Vuex)
 
-// имитация запроса на сервер, возвращает промис с задержкой
-const fetch = (time) => {
-  return new Promise((resolve) => { 
-    setTimeout(() => {
-      resolve(reactions)
-    }, time)
-  })
-}
-
 export default new Vuex.Store({
-  state: {
-    reactions: []
-  },
-  getters: {
-    reactions(state) {
-      return state.reactions
-    }
-  },
-  mutations: {
-    SET_REACTIONS(state, reactions) {
-      state.reactions = reactions
-    }
-  },
-  actions: {
-    async loadReactions({ commit }, time) {
-      try {
-        const reactions = await fetch(time)
-        commit('SET_REACTIONS', reactions)
-      } catch(error) {
-        console.error(error)
-      }
-    }
-  }
+  modules: { reactions }
 })
---
+++ src/store/modules/reactions.js
@@ -0,0 +1,37 @@
+// импортируем данные 'сервера'
+import reactions from '../../mocks/reactions'
+
+// имитация запроса на сервер, возвращает промис с задержкой
+const fetch = (time) => {
+  return new Promise((resolve) => { 
+    setTimeout(() => {
+      resolve(reactions)
+    }, time)
+  })
+}
+
+export default {
+  state: {
+    reactions: []
+  },
+  getters: {
+    reactions(state) {
+      return state.reactions
+    }
+  },
+  mutations: {
+    SET_REACTIONS(state, reactions) {
+      state.reactions = reactions
+    }
+  },
+  actions: {
+    async loadReactions({ commit }, time) {
+      try {
+        const reactions = await fetch(time)
+        commit('SET_REACTIONS', reactions)
+      } catch(error) {
+        console.error(error)
+      }
+    }
+  }
+}
