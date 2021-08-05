a2d57af75e00c8e75515573aec64ce549414c5b4
gumerova.e <gumerova.e@etton.ru>
2021-08-05 09:17:52 +0300

    lesson-9: add fetch function to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// импортируем данные 'сервера'
+import reactions from '../mocks/reactions'
 
 Vue.use(Vuex)
+
+// имитация запроса на сервер, возвращает промис с задержкой
+const fetch = () => {
+  new Promise((resolve) => { 
+    setTimeout(() => {
+      resolve(reactions)
+    }, 1000)
+  })
+}
 
 export default new Vuex.Store({
   state: {
     count: 0
   },
+  getters: {},
   mutations: {
     increment (state) {
       state.count++
+    }
+  },
+  actions: {
+    async loadReactions() {
+      try {
+        const reactions = await fetch()
+        console.log(reactions);
+      } catch(error) {
+        console.error(error)
+      }
     }
   }
 })
