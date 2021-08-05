c4e6389a9904a3930471d83e5f31f822fde61ff1
gumerova.e <gumerova.e@etton.ru>
2021-08-05 12:50:20 +0300

    lesson-10: install vue-js-modal plugin

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router/index'
 import store from './store/index'
+import vmodal from 'vue-js-modal'
 
 Vue.use(VueRouter)
+Vue.use(vmodal)
 
 import '@/assets/styles/main.css'
 
