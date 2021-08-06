52644ef30694b34cdc45de4cd3aa0de628ed64cc
gumerova.e <gumerova.e@etton.ru>
2021-08-06 11:30:03 +0300

    lesson-10: add user modal to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import reactions from './modules/reactions'
+import user from './modules/user'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: { reactions }
+  modules: { 
+    reactions,
+    user
+  }
 })
---
+++ src/store/modules/user.js
@@ -0,0 +1,26 @@
+export default {
+  state: {
+    user: null
+  },
+  getters: {
+    getUser(state) {
+      return state.user
+    }
+  },
+  mutations: {
+    SET_USER(state, payload) {
+      state.user = payload
+    },
+    DELETE_USER(state) {
+      state.user = null
+    }
+  },
+  actions: {
+    setUser({ commit }, payload) {
+      commit('SET_USER', payload)
+    },
+    deleteUser({ commit }, payload) {
+      commit('DELETE_USER', payload)
+    }
+  }
+}
